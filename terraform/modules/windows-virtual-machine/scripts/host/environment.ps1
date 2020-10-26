cd0f66fd0118e43daa8d99925c6a1eecc94f7620
Eric van Wijk <eric@van-wijk.com>
2020-10-26 07:57:50 +0100

    Updated environment variables

--- terraform/modules/windows-virtual-machine/scripts/host/environment.ps1
+++ terraform/modules/windows-virtual-machine/scripts/host/environment.ps1
@@ -5,6 +5,8 @@ $env:ASPNETCORE_ENVIRONMENT="Development"
 # Terraform environment variables
 $env:TF_VAR_backend_resource_group="${tf_backend_resource_group}"
 $env:TF_VAR_backend_storage_account="${tf_backend_storage_account}"
+$env:TF_VAR_backend_storage_container="${tf_backend_storage_container}"
+
 $env:ARM_SUBSCRIPTION_ID="${arm_subscription_id}"
 $env:ARM_TENANT_ID="${arm_tenant_id}"
 $loginError = $(az account show -o none 2>&1)
