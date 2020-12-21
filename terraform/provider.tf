6e3e9c83a385e3c869967ccf9cf0cbf3ba1f624c
Eric van Wijk <eric@van-wijk.com>
2020-12-21 14:59:51 +0100

    Revert to Terraform 0.13

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    azurerm                    = "~> 2.19"
+    azurerm                    = "~> 2.41"
     cloudinit                  = "~> 2.1.0"
     external                   = "~> 2.0.0"
     http                       = "~> 2.0.0"
@@ -10,16 +10,18 @@ terraform {
     time                       = "~> 0.6"
     tls                        = "~> 2.2"
   }
-  required_version             = "~> 0.13"
+  required_version             = "~> 0.13.0"
 }
 
-
 # Microsoft Azure Resource Manager Provider
-provider "azurerm" {
-    features {
-        virtual_machine {
-            # Don't do this in production
-            delete_os_disk_on_deletion = true
-        }
+provider azurerm {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
     }
+    virtual_machine {
+      # Don't do this in production
+      delete_os_disk_on_deletion = true
+    }
+  }
 }
