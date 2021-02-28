32f109bbac1072f0d4b4d82011fa1679ce4240b3
Eric van Wijk <eric@van-wijk.com>
2021-02-28 20:13:47 +0100

    Do not set ARM_ACCESS_KEY (#6)

--- terraform/modules/windows-virtual-machine/scripts/host/environment.ps1
+++ terraform/modules/windows-virtual-machine/scripts/host/environment.ps1
@@ -9,7 +9,7 @@ $env:TF_VAR_backend_storage_container="${tf_backend_storage_container}"
 
 $env:ARM_SUBSCRIPTION_ID="${arm_subscription_id}"
 $env:ARM_TENANT_ID="${arm_tenant_id}"
-$loginError = $(az account show -o none 2>&1)
-if (!$loginError) {
-    $env:ARM_ACCESS_KEY=$(az storage account keys list -n $env:TF_VAR_backend_storage_account -g $env:TF_VAR_backend_resource_group --subscription $env:ARM_SUBSCRIPTION_ID --query "[0].value" -o tsv)
-}
+# $loginError = $(az account show -o none 2>&1)
+# if (!$loginError) {
+#     $env:ARM_ACCESS_KEY=$(az storage account keys list -n $env:TF_VAR_backend_storage_account -g $env:TF_VAR_backend_resource_group --subscription $env:ARM_SUBSCRIPTION_ID --query "[0].value" -o tsv)
+# }
