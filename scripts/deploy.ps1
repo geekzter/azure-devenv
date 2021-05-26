8307b9fbed164d205e0cc7118210853be3117443
Eric van Wijk <eric@van-wijk.com>
2021-05-26 08:10:24 +0200

    AzLogin checks tenantID

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -55,7 +55,7 @@ if (($workspace -ieq "prod") -and $Force) {
 try {
     $tfdirectory = (Get-TerraformDirectory)
     Push-Location $tfdirectory
-    AzLogin
+    AzLogin -DisplayMessages
     # Print version info
     terraform -version
 
