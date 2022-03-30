95529d3966893fdce10362cbdd543c0696d20557
Eric van Wijk <eric@van-wijk.com>
2022-03-30 11:30:11 +0200

    Run bootstrap interactive

--- terraform/modules/windows-virtual-machine/scripts/host/setup_windows_vm.ps1
+++ terraform/modules/windows-virtual-machine/scripts/host/setup_windows_vm.ps1
@@ -44,7 +44,7 @@ if ($virtualNetworkID) {
 $localBatchScript = "$env:PUBLIC\setup.cmd"
 $localPSScript = $PSCommandPath
 Write-Output "PowerShell.exe -ExecutionPolicy Bypass -Noexit -File $localPSScript" | Out-File -FilePath $localBatchScript -Encoding OEM
-schtasks.exe /create /f /rl HIGHEST /sc onlogon /tn "Bootstrap" /tr $localBatchScript
+schtasks.exe /create /f /rl HIGHEST /ru interactive /sc onlogon /tn "Bootstrap" /tr $localBatchScript
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoLogonCount -Value 99
 
 # Create shortcut
